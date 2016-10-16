56b142da689c9268b4f17700ad3f950f5c823434
daniel parkes <liquidsmail@gmail.com>
2016-10-16 03:32:29 +0200

    first commit

---
+++ roles/dbserver/dump/dump.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `test` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL DEFAULT '',
+  `pass` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`aid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `test` (`aid`, `user`, `pass`) VALUES
+('1', 'test', 'test');
